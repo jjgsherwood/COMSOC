62b873354f8af12e547a5f8368b72235fbab70b9
Thijs Kuipers <kuipersthijs@gmail.com>
2021-05-10 17:00:46 +0200

    profile already exists woops

--- mechanism.py
+++ mechanism.py
@@ -1,3 +1,4 @@
+# %%
 import numpy as np
 
 class Mechanism():
@@ -5,11 +6,37 @@ class Mechanism():
     def __init__(self, profile):
         self.__profile = profile
 
+        self.__mechanisms = {
+            "max_approval": self.__max_approval,
+            "greedy_approval": self.__greedy_approval
+        }
+
+
+    def __max_approval(self):
+        raise NotImplementedError()
+
+
+    def __greedy_approval(self):
+        raise NotImplementedError()
+
+
+    def solve(self, mechanism="greedy_approval"):
+        try:
+            return self.__mechanisms[mechanism]()
+        except KeyError():
+            raise ValueError(f"Mechanism: unknown mechanism '{mechanism}.'")
+
+    
+    def __repr__(self):
+        return self.__profile
+
 
 
 
 if __name__ == '__main__':
-    from profile import Profile
+    from approval_profile import Profile
 
     test = Mechanism(Profile("data/poland_warszawa_2018_praga-poludnie.pb"))
 
+
+# %%
