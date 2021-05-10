e9e6c8c777b364c0d49332a728afb23bf2bc04b8
Thijs Kuipers <kuipersthijs@gmail.com>
2021-05-10 16:37:57 +0200

    profile update and mechanim

---
+++ mechanism.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+class Mechanism():
+
+    def __init__(self, profile):
+        self.__profile = profile
+
+
+
+
+if __name__ == '__main__':
+    from profile import Profile
+
+    test = Mechanism(Profile("data/poland_warszawa_2018_praga-poludnie.pb"))
+
